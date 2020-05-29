9a7dc2fcb8dda6b68a157757fa583c9f4030991e
Grace Luo <graceluo@berkeley.edu>
2020-05-29 11:07:11 -0700

    Improve old gameplay and add new features such as creating a new game

--- portfolio/src/main/webapp/components/game-component.js
+++ portfolio/src/main/webapp/components/game-component.js
@@ -2,20 +2,23 @@
     Game. 
  */
 import {LitElement, html} from 'https://unpkg.com/@polymer/lit-element/lit-element.js?module';
+import {equalSets, randomInt, shuffle, flatten, containsElement, readTxt, fetchJSON} from './utils.js';
 
 export class GameComponent extends LitElement {
   static get properties() {
     return {
       boardLength: {type: Number},
-      boardIndices: {type: Array},
-      board: {type: Array}, // 1D array of length boardSize * boardSize
-      wordArrays: {type: Array},
-      baseWords: {type: Array},
+      board: {type: Array},
+      wordArrays: {type: Array}, 
+      baseWords: {type: Array}, 
       selectedWords: {type: Set},
-      selectedSet: {type: Set},
+      selectedSet: {type: Set}, 
       numSolved: {type: Number},
+      gamesSolved: {type: Number},
       solvedColors: {type: Array},
       defaultColor: {type: String},
+      placeholder: {type: String},
+      errorMessage: {type: String},
     };
   }
 
@@ -26,33 +29,42 @@ export class GameComponent extends LitElement {
     this.selectedWords = new Set();
     this.selectedSet = new Set();
     this.numSolved = 0;
+    this.gamesSolved = 0;
+    this.placeholder = "--";
+    this.errorMessage = "";
 
-    // The first color is the default tile color
     this.solvedColors = ["#e67e22", "#8e44ad", "#2ecc71", "#e74c3c", "#f1c40f", "#16a085", "#d35400", "#9b59b6", "#c0392b"];
     this.defaultColor = "#ffffff";
-    this.boardIndices = [];
-    for(let i=0; i<this.boardLength; i+=1) {
-      this.boardIndices.push(i);
-    }
   
   	this.initializeBaseWords();
     this.newBoard();
   }
 
   render() {
+    const boardIndices = [...Array(this.boardLength).keys()];
     return html`        
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.8.2/css/bulma.css" />
       <link rel="stylesheet" href="style.css">            
       <div>
-      	<p class="title">WordSet Game</p>
-        <p>If you think that certain tiles are associated, click on them to create a set! 
-        Sets range from size 1 to ${this.boardLength}. Please wait until the board has finished building 
-        if the cards display "--". </p>
+      	<p class="title">WordGroup Game</p>
+        <p>If you think that certain tiles are associated, click on them to create a group! 
+        Groups range from size 1 to ${this.boardLength}. Please wait until the board has finished building 
+        if the cards display ${this.placeholder}. </p>
+        <p class="has-text-danger">${this.errorMessage}</p>
       	<div id="board">
-          ${this.boardIndices.map(i =>
+          <div class="columns">
+          	<div class="column">
+        			<p>Games solved: ${this.gamesSolved}</p>
+              <p>Groups found this game: ${this.numSolved}</p>
+            </div>
+            <div class="column">
+        			<button class="button is-link is-pulled-right" @click=${this.newBoard}> New Board </button>
+            </div>
+          </div>
+          ${boardIndices.map(i =>
             html`
               <div class="tile is-ancestor">
-                ${this.boardIndices.map(j => {
+                ${boardIndices.map(j => {
                 	let boardWord = this.board[this.boardLength*i+j];
                   return html`
                     <button class="tile card" id=${boardWord} @click=${() => this.handleSelection(this.board[this.boardLength*i+j])}>
@@ -70,9 +82,9 @@ export class GameComponent extends LitElement {
     `;
   }
 
-	initializeTiles(newWord) {
+	findGroup(newWord) {
     for(const wordArray of this.wordArrays) {
-      if(wordArray.indexOf(newWord) > -1) {
+      if(wordArray.containsElement(newWord)) {
       	this.selectedSet = new Set(wordArray);
       }
     }
@@ -80,8 +92,9 @@ export class GameComponent extends LitElement {
     console.log(this.selectedSet);
   }
 
-	allTiles(action) {
-    for (const word of this.selectedWords) {
+	changeGroup(action, group) {
+    console.log(group);
+    for (const word of group) {
       let tile = this.shadowRoot.getElementById(word);
       if (action === "lock") {
         tile.disabled = true;
@@ -93,8 +106,8 @@ export class GameComponent extends LitElement {
     this.selectedSet = new Set();
   }
 
-  lockTiles() {
-    this.allTiles("lock");
+  lockGroup() {
+    this.changeGroup("lock", this.selectedWords);
     this.numSolved += 1;
   }
 
@@ -104,112 +117,78 @@ export class GameComponent extends LitElement {
     tile.style.background = tileColor;
   }
 
-  voidTiles() {
-    this.allTiles("void");
+  voidGroup() {
+    this.numSolved = 0;
+    this.changeGroup("void", this.selectedWords);
+  }
+
+  finishGame() {
+    this.gamesSolved += 1;
+    console.log(this.board);
+    this.changeGroup("void", this.board);
+  	this.newBoard();
   }
 
   handleSelection(newWord) {
-    this.selectedWords.add(newWord);
-    if(this.selectedSet.size === 0) {
-      this.initializeTiles(newWord);
-    } else if (this.equalSets(this.selectedWords, this.selectedSet)) {
-      this.colorTile(newWord);
-      this.lockTiles();
-    } else if (this.selectedSet.has(newWord)) {
-      this.colorTile(newWord);
-    } else {
-      this.voidTiles();
-    }
-  }
-
-	equalSets(setA, setB) {
-    if (setA.size !== setB.size) { 
-      return false;
-    }
-    for (let a of setA) {
-      if (!setB.has(a)) {
-        return false;
+    if (newWord !== this.placeholder) {
+      this.selectedWords.add(newWord);
+      if(this.selectedSet.size === 0) {
+        this.findGroup(newWord);
+      } else if (equalSets(this.selectedWords, this.selectedSet)) {
+        this.colorTile(newWord);
+        this.lockGroup();
+      } else if (this.selectedSet.containsElement(newWord)) {
+        this.colorTile(newWord);
+      } else {
+        this.voidGroup();
+      }
+      if(this.numSolved === this.wordArrays.length) {
+        this.finishGame();
       }
     }
-    return true;
   }
-
-	// Random int between min (inclusive) and max (exclusive)
-	randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  } 
-
-  shuffle(arr) {
-    return arr
-      .map((item) => ({key: Math.random(), value: item}))
-      .sort((itemOne, itemTwo) => itemOne.key - itemTwo.key)
-      .map((item) => item.value)
-  }
-
-  readTxt(filePath){
-    let text = "";
-    try {
-      let request = new XMLHttpRequest();
-      request.open("GET", filePath, false);
-      request.send(null);
-      text = request.responseText;
-    } catch(error) {
-      throw error;
-    }
-    return text;
-	}
 
 	initializeBaseWords() {
-    let nounList = this.readTxt("../assets/nouns.txt");
+    let nounList = readTxt("../assets/nouns.txt");
     this.baseWords = nounList.split("\n");
-    this.baseWords = this.shuffle(this.baseWords);
-  }
-
-  retrieveJSON(url) {
-    const proxyURL = "https://cors-anywhere.herokuapp.com/"; // Bypass 'Access-Control-Allow-Origin' error in development
-    return fetch(proxyURL + url)
-      .then(res => {
-        return res.json()
-      })
-      .then((out) => {
-        return out;
-      })
-      .catch(err => { 
-        throw err; 
-      });
+    this.baseWords = shuffle(this.baseWords);
   }
 
   async collectWords(url) {
-    let wordArray = await this.retrieveJSON(url);
-    let words = wordArray.map(wordInfo => wordInfo["word"]);
-    return words;
+    try {
+      let wordArray = await fetchJSON(url);
+      let words = wordArray.map(wordInfo => wordInfo["word"]);
+      return words;
+    } catch(e) {
+      this.errorMessage = "Oh no! We couldn't fetch the words from the database. Please try again in 10 minutes.";
+    }
   }	
   
 	async createwordArrays() {
     let numFreeTiles = this.boardLength * this.boardLength;
     for (const topic of this.baseWords) {
       if (numFreeTiles > 0) {
-        let maxSize = Math.min(numFreeTiles, this.randomInt(2, this.boardLength)); 
-        let setURL = "https://api.datamuse.com/words?topics=" + topic + "&max=" + maxSize; 
-        let newSet = await this.collectWords(setURL);
-        this.wordArrays.push(newSet);
-        numFreeTiles -= newSet.length;
+        let maxSize = Math.min(numFreeTiles, randomInt(2, this.boardLength)); 
+        let groupURL = "https://api.datamuse.com/words?topics=" + topic + "&max=" + maxSize; 
+        let newGroup = await this.collectWords(groupURL);
+        this.wordArrays.push(newGroup);
+        numFreeTiles -= newGroup.length;
         const index = this.baseWords.indexOf(topic);
         this.baseWords.splice(index, 1);
       }
     }
   } 
 
-	// Use async and await since retrieveJSON returns a Promise until resolved
   async newBoard() {
+    this.voidGroup();
     this.board = [];
     for (let i=1; i<=this.boardLength * this.boardLength; i++) {
-      this.board.push("--")
+      this.board.push(this.placeholder)
     }
-	
+    this.baseWords = shuffle(this.baseWords);
     await this.createwordArrays();
-    this.board = this.wordArrays.reduce(function(arrOne, arrTwo){return arrOne.concat(arrTwo);}, []); // Flatten 2D array
-    this.board = this.shuffle(this.board);
+    this.board = flatten(this.wordArrays);
+    this.board = shuffle(this.board);
   }
   
 }
