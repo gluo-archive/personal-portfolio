5ffc463b62c774e6ec831633e4aa84335ee1997b
Grace Luo <36020488+g-luo@users.noreply.github.com>
2020-05-29 11:02:20 -0700

    Reformat to two spaces

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -2,14 +2,14 @@
 *	White out screen while backgrounds scripts are loading.
 */
 document.onreadystatechange = function () {
-  var state = document.readyState
+	var state = document.readyState
   if (state == 'interactive') {
-       document.getElementById('content').style.visibility="hidden";
+    document.getElementById('content').style.visibility="hidden";
   } else if (state == 'complete') {
-      setTimeout(function(){
-         document.getElementById('interactive');
-         document.getElementById('load').style.visibility="hidden";
-         document.getElementById('content').style.visibility="visible";
-      },1000);
+  	  setTimeout(function(){
+        document.getElementById('interactive');
+        document.getElementById('load').style.visibility="hidden";
+        document.getElementById('content').style.visibility="visible";
+    	}, 1000);
   }
 }
