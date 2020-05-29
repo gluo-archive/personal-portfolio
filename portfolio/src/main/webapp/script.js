d903d043e3cbe2822c72412093c06be6f5337a9a
Grace Luo <36020488+g-luo@users.noreply.github.com>
2020-05-29 11:05:09 -0700

    More formatting fixes

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,12 +1,12 @@
 /*
 *	White out screen while backgrounds scripts are loading.
 */
-document.onreadystatechange = function () {
+document.onreadystatechange = function() {
 	var state = document.readyState
   if (state == 'interactive') {
     document.getElementById('content').style.visibility="hidden";
   } else if (state == 'complete') {
-  	  setTimeout(function(){
+  	  setTimeout(function() {
         document.getElementById('interactive');
         document.getElementById('load').style.visibility="hidden";
         document.getElementById('content').style.visibility="visible";
