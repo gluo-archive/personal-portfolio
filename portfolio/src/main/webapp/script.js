02447fda9a4194fd5004c96ad13db9fb6f6fec34
Grace Luo <graceluo@berkeley.edu>
2020-06-01 13:37:45 -0700

    Add Week 3 Step 3, json

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,7 +1,17 @@
 function getServletData() {
-  fetch('/data')
-  	.then(response => response.text())
-    .then((message) => {
-    	document.getElementById('message-container').innerText = message;
-  	});
+  fetch("/data").then(response => response.json()).then((commentList) => {
+  	const commentListElement = document.getElementById("comment-container");
+    console.log(commentList);
+    commentList.map((comment) => {
+      commentListElement.appendChild(
+          createListElement(comment));
+      });
+    });
+}
+
+/** Create an <li> element containing text. */
+function createListElement(text) {
+  const liElement = document.createElement("li");
+  liElement.innerText = text;
+  return liElement;
 }
