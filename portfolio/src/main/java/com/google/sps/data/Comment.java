8a2145175a5a992d1be83597c8bc411b1e0d1aa5
Grace Luo <graceluo@berkeley.edu>
2020-06-04 23:47:48 +0000

    Add comment sentiment analysis

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -6,11 +6,13 @@ public final class Comment {
   private final String title;
   private final String content;
   private final long timestamp;
+  private final String sentiment;
 
-  public Comment(long id, String title, String content, long timestamp) {
+  public Comment(long id, String title, String content, long timestamp, String sentiment) {
     this.id = id;
     this.title = title;
     this.content = content;
     this.timestamp = timestamp;
+    this.sentiment = sentiment;
   }
 }
